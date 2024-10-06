58405564f24007b352ca86163346dd3f21fd837e
gabriel dawa <168088828+gabrieldawa@users.noreply.github.com>
2024-10-05 23:48:33 -0300

    Update Compiler.java
    added pt

--- main/Compiler.java
+++ main/Compiler.java
@@ -1,18 +1,18 @@
-// compiler for option 1 of school project by Gabriel Dawa - 20231ss.tds_q0065
+// compilador para a opção 1 da tarefa de fundamentos ~ by Gabriel Dawa - 20231ss.tds_q0065
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Compiler {
     public static void main(String[] args) throws Exception{
-        File input = new File("input.txt"); // turns the input.txt file into a file object
-        Scanner sc = new Scanner(input); // create scanner that reads the file object    
-        String tempIn, tempOut = ""; // sets the temporary input and output variables 
+        File input = new File("input.txt"); // pega o arquivo "input.txt" e cria o objeto input
+        Scanner sc = new Scanner(input); // cria o scanner que lê o objeto input
+        String tempIn, tempOut = ""; // cria variáveis temporárias de entrada e saída 
 
-        while(sc.hasNext()){ // while theres text to be read
-            tempIn = sc.next().trim(); // read the text, set it to the var temp and remove whitespace
-
-            switch(tempIn){// checks what the contents of the text is and adjusts accordingly 
+        while(sc.hasNext()){ // enquanto há texto a ser lido
+            tempIn = sc.next().trim(); // lê o texto parte por parte
+            
+            switch(tempIn){// pega a parte do texto em tempIn, analiza e age conforme necessário, concatenado tudo em tempOut 
                 case "LOAD":  tempOut += "100"; break;
                 case "PRINT": tempOut += "101"; break;
                 case "HALT":  tempOut += "102"; break;
@@ -22,15 +22,15 @@ public class Compiler {
                 case "JNZ":   tempOut += "106"; break;
                 case "JZ":    tempOut += "107"; break;
                 case "SET":   tempOut += "108"; break;
-                default: // ignore "R" or ","
+                default: // ignora "R" or ","
                     tempOut += (tempIn.startsWith("R")) ? (tempIn.endsWith(",")) ? tempIn.substring(1, tempIn.length() - 1) : tempIn.substring(1) : tempIn; 
                     break;
-            }   tempOut += (sc.hasNext()) ? "\n" : ""; // breaks every line sans the last one
-        }   sc.close();// closes scanner
+            }   tempOut += (sc.hasNext()) ? "\n" : ""; // quebra todas as linhas menos a última
+        }   sc.close();// fecha scanner
 
-        File output = new File("output.txt"); // creates new file output.txt
-        FileWriter wr = new FileWriter(output); // creates filewriter that writes to output.txt
-        wr.write(tempOut); // writes output to file
-        wr.close();//closes writer 
+        File output = new File("output.txt"); // cria arquivo output.txt
+        FileWriter wr = new FileWriter(output); // cria filewriter que escreve em output.txt
+        wr.write(tempOut); // escreve tempOut no arquivo output
+        wr.close();// fecha filewriter 
     }
 }
